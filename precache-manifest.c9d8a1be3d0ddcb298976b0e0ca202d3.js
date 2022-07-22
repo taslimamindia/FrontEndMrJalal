a650d336d75ca50a24feef1eaa17ed57946bf7a1
taslimamindia <mamadoutaslima.diallo@usmba.ac.ma>
2022-07-22 22:22:53 +0100

    Updates

---
+++ precache-manifest.c9d8a1be3d0ddcb298976b0e0ca202d3.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "fbbc8e70ab0884a5b10d",
+    "url": "/FrontEndMrJalal/static/css/main.af95e880.chunk.css"
+  },
+  {
+    "revision": "fbbc8e70ab0884a5b10d",
+    "url": "/FrontEndMrJalal/static/js/main.fbbc8e70.chunk.js"
+  },
+  {
+    "revision": "fbd0924dd8ee322e9bc2",
+    "url": "/FrontEndMrJalal/static/js/1.fbd0924d.chunk.js"
+  },
+  {
+    "revision": "754c6444745efe9b8116",
+    "url": "/FrontEndMrJalal/static/css/2.b1106457.chunk.css"
+  },
+  {
+    "revision": "754c6444745efe9b8116",
+    "url": "/FrontEndMrJalal/static/js/2.754c6444.chunk.js"
+  },
+  {
+    "revision": "1cc8d3ed0e5fb9e1cabb",
+    "url": "/FrontEndMrJalal/static/js/runtime~main.1cc8d3ed.js"
+  },
+  {
+    "revision": "9774a586b85dd53b1804b845cc8d675e",
+    "url": "/FrontEndMrJalal/static/media/fst.9774a586.png"
+  },
+  {
+    "revision": "23c975a3992114fc47012174ba2a38bd",
+    "url": "/FrontEndMrJalal/static/media/not-found.23c975a3.svg"
+  },
+  {
+    "revision": "f9f23ffd1c859a0180178a7fe73cc91a",
+    "url": "/FrontEndMrJalal/index.html"
+  }
+];
